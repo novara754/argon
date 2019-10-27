cac781b2f0914a9db911335df654e7fd54b0617f
vzwGrey <vzwgrey@gmail.com>
2019-10-27 14:10:25 +0100

    style: move precedences into cpp, change name to snake_case

--- src/Facts.cpp
+++ src/Facts.cpp
@@ -1,22 +1,54 @@
 #include "Facts.h"
 
+static const TokenKind precedence_order[] = {
+	TokenKind::Or,
+	TokenKind::And,
+	TokenKind::BangEqual,
+	TokenKind::EqualEqual,
+	TokenKind::GreaterThan,
+	TokenKind::GreaterThanEqual,
+	TokenKind::LessThan,
+	TokenKind::LessThanEqual,
+	TokenKind::Minus,
+	TokenKind::Plus,
+	TokenKind::Slash,
+	TokenKind::Star,
+};
+
+static const BinaryOperator binary_operators[] = {
+	{ TokenKind::Plus, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value + right.int_value; } },
+	{ TokenKind::Minus, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value - right.int_value; } },
+	{ TokenKind::Star, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value * right.int_value; } },
+	{ TokenKind::Slash, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value / right.int_value; } },
+	{ TokenKind::LessThan, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value < right.int_value; } },
+	{ TokenKind::LessThanEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value <= right.int_value; } },
+	{ TokenKind::GreaterThan, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value > right.int_value; } },
+	{ TokenKind::GreaterThanEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value >= right.int_value; } },
+	{ TokenKind::And, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value && right.bool_value; } },
+	{ TokenKind::Or, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value || right.bool_value; } },
+	{ TokenKind::EqualEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value == right.int_value; } },
+	{ TokenKind::BangEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value != right.int_value; } },
+	{ TokenKind::EqualEqual, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value == right.bool_value; } },
+	{ TokenKind::BangEqual, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value != right.bool_value; } },
+};
+
 int GetBinaryOperatorPrecedence(TokenKind kind)
 {
-	auto itr = std::find(std::begin(PrecedenceOrder), std::end(PrecedenceOrder), kind);
-	if (itr == std::end(PrecedenceOrder))
+	auto itr = std::find(std::begin(precedence_order), std::end(precedence_order), kind);
+	if (itr == std::end(precedence_order))
 		return -1;
 	else
-		return std::distance(PrecedenceOrder, itr);
+		return std::distance(precedence_order, itr);
 }
 
 std::optional<BinaryOperator> GetBinaryOperator(TokenKind op, ValueKind left, ValueKind right)
 {
-	auto itr = std::find_if(std::begin(BinaryOperators), std::end(BinaryOperators), [op, left, right](const BinaryOperator &bin_op)
+	auto itr = std::find_if(std::begin(binary_operators), std::end(binary_operators), [op, left, right](const BinaryOperator &bin_op)
 	{
 		return bin_op.op == op && bin_op.left == left && bin_op.right == right;
 	});
 
-	return itr != std::end(BinaryOperators) ? *itr : std::optional<BinaryOperator>();
+	return itr != std::end(binary_operators) ? *itr : std::optional<BinaryOperator>();
 }
 
 bool CharacterIsNumber(char c) {
--- src/Facts.h
+++ src/Facts.h
@@ -14,38 +14,6 @@ struct BinaryOperator
 	std::function<Value(Value, Value)> Operate;
 };
 
-const TokenKind PrecedenceOrder[] = {
-	TokenKind::Or,
-	TokenKind::And,
-	TokenKind::BangEqual,
-	TokenKind::EqualEqual,
-	TokenKind::GreaterThan,
-	TokenKind::GreaterThanEqual,
-	TokenKind::LessThan,
-	TokenKind::LessThanEqual,
-	TokenKind::Minus,
-	TokenKind::Plus,
-	TokenKind::Slash,
-	TokenKind::Star,
-};
-
-const BinaryOperator BinaryOperators[] = {
-	{ TokenKind::Plus, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value + right.int_value; } },
-	{ TokenKind::Minus, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value - right.int_value; } },
-	{ TokenKind::Star, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value * right.int_value; } },
-	{ TokenKind::Slash, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value / right.int_value; } },
-	{ TokenKind::LessThan, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value < right.int_value; } },
-	{ TokenKind::LessThanEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value <= right.int_value; } },
-	{ TokenKind::GreaterThan, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value > right.int_value; } },
-	{ TokenKind::GreaterThanEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value >= right.int_value; } },
-	{ TokenKind::And, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value && right.bool_value; } },
-	{ TokenKind::Or, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value || right.bool_value; } },
-	{ TokenKind::EqualEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value == right.int_value; } },
-	{ TokenKind::BangEqual, ValueKind::Integer, ValueKind::Integer, [](auto left, auto right) { return left.int_value != right.int_value; } },
-	{ TokenKind::EqualEqual, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value == right.bool_value; } },
-	{ TokenKind::BangEqual, ValueKind::Boolean, ValueKind::Boolean, [](auto left, auto right) { return left.bool_value != right.bool_value; } },
-};
-
 int GetBinaryOperatorPrecedence(TokenKind kind);
 std::optional<BinaryOperator> GetBinaryOperator(TokenKind op, ValueKind left, ValueKind right);
 
